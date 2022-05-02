5775fc1b367dfd9a59348126e0c8ae08526e5d06
rnavas15 <raulnavasgs@hotmail.com>
2022-05-02 10:07:26 -0300

    ultimo antes de angular

--- src/main/java/com/Stock/Stock/Controladores/ProductoRestController.java
+++ src/main/java/com/Stock/Stock/Controladores/ProductoRestController.java
@@ -5,10 +5,40 @@
  */
 package com.Stock.Stock.Controladores;
 
+import com.Stock.Stock.Entidades.Producto;
+import com.Stock.Stock.Servicios.ProductoServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author rnavas
  */
+
+@RestController
+
+   
+
+@RequestMapping("/productos2")
+
 public class ProductoRestController {
+    
+    @Autowired
+    ProductoServicio productoServicio;
+    
+
+    @GetMapping(path="/test")
+    public ResponseEntity<List<Producto>> ListaDeProductos(){
+        System.out.println("LLAMO AL CONTROLADOR");
+        List<Producto> productos=productoServicio.buscarProductos();
+        return new ResponseEntity<>(productos,HttpStatus.OK);
+    }
     
 }
--- src/main/java/com/Stock/Stock/StockApplication.java
+++ src/main/java/com/Stock/Stock/StockApplication.java
@@ -1,8 +1,14 @@
 package com.Stock.Stock;
 
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class StockApplication {
@@ -13,8 +19,23 @@ public class StockApplication {
                
              
 	}
+        @Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
       
-         
+   
            
 
 }
